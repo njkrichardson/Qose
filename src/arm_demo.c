0d741c0ea71c8d3e7c835e3290aeaac4edbd9652
Nick Richardson <nrichardson@g.hmc.edu>
2022-07-26 15:43:27 -0400

    add full demo and binary

--- src/arm_demo.c
+++ src/arm_demo.c
@@ -33,20 +33,66 @@
 #include "io_utils.h"
 #include "utils.h"
 
-int main()
+static void usage(char *program) {
+    printf( "Usage:\n" );
+    printf( " %s genkey [keyname]\n", program );
+    printf( " %s genkey [keyname] [parameter set]\n", program );
+    printf( " %s sign [keyname] [files to sign]\n", program );
+    printf( " %s verify [keyname] [files to verify]\n", program );
+    printf( " %s advance [keyname] [amount of advance]\n", program );
+}
+
+int main(int argc, char** argv)
 {
+    if (argc < 2) 
+    {
+        usage(argv[0]);
+        return 0;
+    }
     // --- parameter set and default configuration
     const char* parameters = "20/8,10/8"; 
+    const char* key_name = "default"; 
 
     // --- generate a public/private keypair 
-    const char* key_name = "default"; 
-    int result = keygen(key_name, parameters); 
+    if (0 == strcmp( argv[1], "genkey" ))
+    {
+        if (!keygen(key_name, parameters))
+        {
+            printf("Error generating key\n."); 
+        }
+    }
 
-    // -- load the private key and sign files
-    
+    // --- load the private key and sign files
+    if (0 == strcmp(argv[1], "sign")) 
+    {
+        if (argc < 4) {
+            printf( "Error: missing keyname and file argument\n" );
+            usage(argv[0]);
+            return 0;
+        }
+        if (!sign( argv[2], &argv[3] )) {
+            printf( "Error signing\n" );
+        }
+        return 0;
+    }
+
     // -- given the public key, verify the generated signatures 
+    if (0 == strcmp( argv[1], "verify" )) 
+    {
+        if (argc < 4) 
+        {
+            printf( "Error: mssing keyname and file argument\n" );
+            usage(argv[0]);
+            return 0;
+        }
+        if (!verify( argv[2], &argv[3] )) 
+        {
+            printf( "Error verifying\n" );
+        }
+        return 0;
+    }
+
+    usage(argv[0]);
     
-    // -- advance the private key 
-    
-    return 0; 
+    return 1; 
 }
